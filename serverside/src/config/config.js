1719875f140b198f125e0e2862ca515330ba98ae
anasircft <anasircft@gmail.com>
2024-06-07 13:51:05 +0500

    BUG FIX | config updated

--- serverside/src/config/config.js
+++ serverside/src/config/config.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
+require('dotenv').config();
 // Connect to MongoDB
-mongoose.connect('mongodb://localhost:27017/Test123', {
+mongoose.connect(process.env.MONGODB_URL, {
   // useNewUrlParser: true,
   // useUnifiedTopology: true,
-});
+}).then(()=>console.log("Database Connected"))
