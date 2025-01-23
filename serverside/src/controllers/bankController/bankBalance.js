422ea0bc2575ac9e59d691fe6ee7f582cc097d0a
abdullah <anasircft@gmail.com>
2025-01-23 18:04:50 +0500

    OE-3 | Backend | Code Refactor | Database Optimization | BANK_LIST

---
+++ serverside/src/controllers/bankController/bankBalance.js
@@ -0,0 +1,52 @@
+const BankBalance = require("../../models/bankModel/bankBalance");
+const CheckBank = require("../../middleware/checkBank");
+
+module.exports = {
+  createBankBalance: async (req, res) => {
+    const { balance, bank } = req.body;
+
+    try {
+      if (!balance) {
+        return res.status(404).json({
+          status: "error",
+          message: "Balance is Required",
+        });
+      }
+
+      if (!bank) {
+        return res.status(404).json({
+          status: "error",
+          message: "Bank Account is Required",
+        });
+      }
+
+      const { bankId } = await CheckBank.checkBank(req, res, bank);
+
+      if (bankId === null) {
+        return res.status(404).json({
+          status: "error",
+          message: "Bank Account not found",
+        });
+      }
+
+      const createOrUpdateBankBalance = await BankBalance.findOneAndUpdate(
+        { bank: bankId },
+        {
+          $set: { balance: balance },
+        },
+        {
+          new: true,
+          upsert: true,
+        }
+      );
+
+      res.status(201).json({
+        status: "success",
+        message: "Bank Balance created successfully",
+        data: createOrUpdateBankBalance,
+      });
+    } catch (err) {
+      res.status(500).json({ message: err });
+    }
+  },
+};
--- serverside/src/models/bankModel/bankBalance.js
+++ serverside/src/models/bankModel/bankBalance.js
@@ -1,17 +1,18 @@
-const mongoose = require('mongoose')
-const Schema=mongoose.Schema
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
 
 const bankBalanceSchema = new Schema({
-    
-    balance:{
-        type:Number,
-    },
-    bank:{
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'BankListSchema',
-    },
-}
-)
+  balance: {
+    type: Number,
+  },
+  bank: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "BankListSchema",
+  },
+});
 
-const BankBalanceSchema = mongoose.model('BankBalanceSchema',bankBalanceSchema)
-module.exports=BankBalanceSchema;
+const BankBalanceSchema = mongoose.model(
+  "BankBalanceSchema",
+  bankBalanceSchema
+);
+module.exports = BankBalanceSchema;
