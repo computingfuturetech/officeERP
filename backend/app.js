b512b97a7353598f1897036c955466b7a41e8a37
Muhammad Abdullah <135630896+mabdullah136@users.noreply.github.com>
2025-07-17 16:17:59 +0500

    OE-45 | Backend Rate Limiter

--- backend/app.js
+++ backend/app.js
@@ -6,14 +6,16 @@ const router = require("./src/routes");
 const loggerMiddleware = require("./src/middleware/logger.middleware");
 const multerMiddleware = require("./src/middleware/multer.middleware");
 const corsMiddleware = require("./src/middleware/cors.middleware");
+const { rateLimiter } = require("./src/middleware/rateLimiter.middleware");
 
 // Middlewares
 app.use(loggerMiddleware);
+app.use(corsMiddleware);
+app.use(rateLimiter);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(multerMiddleware);
-app.use(corsMiddleware);
 app.use("/public", express.static(path.join(__dirname, "public")));
 
 // Routes
